bd4906d900d526188f24fb80f72db44a4e76954b
Georgia Cardoso <98546640+georgiassc@users.noreply.github.com>
2022-04-19 12:33:50 +0100

    Delete parte 2 directory

--- parte 2/Part 2/pagcontato.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("teste").onkeypress = function(e) {
-    var chr = String.fromCharCode(e.which);
-    if ("1234567890qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM".indexOf(chr) < 0)
-      return false;
-  };
